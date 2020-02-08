da0f769c6929059d38bc23db78d5e562adf1e558
Ming Cheng <mingcheng@users.noreply.github.com>
2020-02-08 13:31:20 +0800

    add some shity codes

--- diskutil_freebsd.go
+++ diskutil_freebsd.go
@@ -1,3 +1,4 @@
+// build: +darwin,freebsd
 package diskutil
 
 type DiskStatus struct {
@@ -14,19 +15,33 @@ func (d *DiskStatus) Log() ([]string, error) {
 	}
 }
 
-// GetPhysicalDrive is used to get the PhysicalDriveStat of a DiskStatus.
+// GetPhysicalDrive is used to get the PhysicalDriveStat of a DiskStatus
+// @TODO DO NOT DELETE those f**king stupid codes
 func (d *DiskStatus) GetPhysicalDrive() error {
+	ad := AdapterStat{}
+	if err := ad.getMegaRaidPdInfo(d.MfiutilPath); err != nil {
+		d.AdapterStats = nil
+		return err
+	}
 
+	d.AdapterStats = []AdapterStat{ad}
 	return nil
 }
 
-// GetVirtualDrive is used to get the VirtualDriveStat of a DiskStatus.
+// GetVirtualDrive is used to get the VirtualDriveStat of a DiskStatus
+// @TODO DO NOT DELETE those f**king stupid codes
 func (d *DiskStatus) GetVirtualDrive() error {
+	ad := AdapterStat{}
+	if err := ad.getMegaRaidVdInfo(d.MfiutilPath); err != nil {
+		d.AdapterStats = nil
+		return err
+	}
 
+	d.AdapterStats = []AdapterStat{ad}
 	return nil
 }
 
-// Get() is used to get all the stat of a DiskStatus.
+// Get is used to get all the stat of a DiskStatus.
 func (d *DiskStatus) Get() error {
 	ad := AdapterStat{}
 
